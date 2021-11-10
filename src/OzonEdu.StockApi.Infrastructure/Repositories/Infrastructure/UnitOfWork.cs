602902c916c0c4978d935cf12d1ba900c152a4b9
Anton Vorontsov <dealwithbotalfred@gmail.com>
2021-11-10 21:28:33 +0300

    Косметические правки.

--- src/OzonEdu.StockApi.Infrastructure/Repositories/Infrastructure/UnitOfWork.cs
+++ src/OzonEdu.StockApi.Infrastructure/Repositories/Infrastructure/UnitOfWork.cs
@@ -8,6 +8,7 @@ using Npgsql;
 using OzonEdu.StockApi.Domain.AggregationModels.DeliveryRequestAggregate;
 using OzonEdu.StockApi.Domain.AggregationModels.StockItemAggregate;
 using OzonEdu.StockApi.Domain.Contracts;
+using OzonEdu.StockApi.Infrastructure.Repositories.Infrastructure.Exceptions;
 using OzonEdu.StockApi.Infrastructure.Repositories.Infrastructure.Interfaces;
 
 namespace OzonEdu.StockApi.Infrastructure.Repositories.Infrastructure
@@ -17,18 +18,18 @@ namespace OzonEdu.StockApi.Infrastructure.Repositories.Infrastructure
         private NpgsqlTransaction _npgsqlTransaction;
         
         private readonly IDbConnectionFactory<NpgsqlConnection> _dbConnectionFactory = null;
-        private readonly IMediator _mediator;
+        private readonly IPublisher _publisher;
         private readonly IRepositoriesHolder _repositoriesHolder;
         private readonly IEntitiesHolder _entitiesHolder;
 
         public UnitOfWork(
             IDbConnectionFactory<NpgsqlConnection> dbConnectionFactory,
-            IMediator mediator,
+            IPublisher publisher,
             IRepositoriesHolder repositoriesHolder,
             IEntitiesHolder entitiesHolder)
         {
             _dbConnectionFactory = dbConnectionFactory;
-            _mediator = mediator;
+            _publisher = publisher;
             _repositoriesHolder = repositoriesHolder;
             _entitiesHolder = entitiesHolder;
         }
@@ -49,13 +50,18 @@ namespace OzonEdu.StockApi.Infrastructure.Repositories.Infrastructure
 
         public async Task SaveChangesAsync(CancellationToken cancellationToken)
         {
+            if (_npgsqlTransaction is null)
+            {
+                throw new NoActiveTransactionStartedException();
+            }
+
             var domainEvents = new Queue<INotification>(
                 _entitiesHolder.UsedEntities
                     .SelectMany(x => x.DomainEvents));
             // Можно отправлять все и сразу через Task.WhenAll.
             while (domainEvents.TryDequeue(out var notification))
             {
-                await _mediator.Publish(notification, cancellationToken);
+                await _publisher.Publish(notification, cancellationToken);
             }
 
             await _npgsqlTransaction.CommitAsync(cancellationToken);
