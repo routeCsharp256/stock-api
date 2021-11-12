5e5f00ea1e75eee152b1fdc4128483f410b311d4
Anton Vorontsov <williams@ozon.ru>
2021-11-13 01:11:14 +0300

    Изменил реализацию item type репозитория.

--- src/OzonEdu.StockApi.Infrastructure/Handlers/StockItemAggregate/GetItemTypesQueryHandler.cs
+++ src/OzonEdu.StockApi.Infrastructure/Handlers/StockItemAggregate/GetItemTypesQueryHandler.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using OzonEdu.StockApi.Domain.AggregationModels.StockItemAggregate;
+using OzonEdu.StockApi.Domain.Contracts;
+using OzonEdu.StockApi.Infrastructure.Models;
 using OzonEdu.StockApi.Infrastructure.Queries.StockItemAggregate;
 using OzonEdu.StockApi.Infrastructure.Queries.StockItemAggregate.Responses;
 
@@ -9,16 +12,30 @@ namespace OzonEdu.StockApi.Infrastructure.Handlers.StockItemAggregate
 {
     public class GetItemTypesQueryHandler : IRequestHandler<GetItemTypesQuery, GetItemTypesQueryResponse>
     {
-        private readonly IStockItemRepository _stockItemRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IItemTypeRepository _itemTypeRepository;
 
-        public GetItemTypesQueryHandler(IStockItemRepository stockItemRepository)
+        public GetItemTypesQueryHandler(IItemTypeRepository itemTypeRepository, IUnitOfWork unitOfWork)
         {
-            _stockItemRepository = stockItemRepository;
+            _itemTypeRepository = itemTypeRepository;
+            _unitOfWork = unitOfWork;
         }
 
-        public Task<GetItemTypesQueryResponse> Handle(GetItemTypesQuery request, CancellationToken cancellationToken)
+        public async Task<GetItemTypesQueryResponse> Handle(GetItemTypesQuery request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            await _unitOfWork.StartTransaction(cancellationToken);
+            var itemTypes = await _itemTypeRepository.GetAllTypes(cancellationToken); 
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return new GetItemTypesQueryResponse
+            {
+                Items = itemTypes.Select(
+                        x => new ItemTypeDto
+                        {
+                            Id = x.Id,
+                            Name = x.ToString()
+                        })
+                    .ToList()
+            };
         }
     }
 }
